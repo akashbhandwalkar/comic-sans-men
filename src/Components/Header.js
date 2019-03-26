86091d1d159646fce17bca77ff93ec8c93eb725b
Akash Bhandwalkar <abakash02@gmail.com>
2019-03-26 18:00:54 +0530

    added header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import Styles from './Header.scss';
+
+
 class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isHidden : true
+        }
+    }
+
+    showHeaderMenu = () =>{
+        this.setState({
+            isHidden : false
+        })
+        
+    }
+
+    hideHeaderMenu = () =>{
+        this.setState({
+            isHidden : true
+        })
+        
     }
 
     render() {
@@ -11,7 +30,46 @@ class Header extends React.Component {
        
         return(<div className="header-container">
                     <div>
-                        ComicSansMen
+                        
+                        ComicSansMen 
+                        {
+                            this.state.isHidden && <i className="fa fa-angle-double-right fa-2x cursor-pointer" aria-hidden="true" onClick = {this.showHeaderMenu}></i> 
+                            
+                        }
+                        
+                        
+                        {
+                            !this.state.isHidden &&
+                        
+                            <div>
+                        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>      
+                            </button>
+
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Portfolio</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">About us</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">Contact</a>
+                                    </li>
+                                    <li>
+                                    <i className="fa fa-angle-double-left fa-2x cursor-pointer" aria-hidden="true" onClick ={this.hideHeaderMenu}></i>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                       
+                        </div>
+                    }
                     </div>
             </div>)
     }
